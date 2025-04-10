bc8cb368f6bfa320d7e15a56142803a21541e8e6
molalign8468 <molaligngetahun229@gmail.com>
2025-04-10 17:00:00 +0300

    Proof of Work

--- The Meme Pool/index.js
+++ The Meme Pool/index.js
@@ -2,30 +2,29 @@ const SHA256 = require("crypto-js/sha256");
 const TARGET_DIFFICULTY =
   BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
 const MAX_TRANSACTIONS = 10;
-
 const mempool = [];
 const blocks = [];
 
 function addTransaction(transaction) {
-  // TODO: add transaction to mempool
   mempool.push(transaction);
 }
 
 function mine() {
-  // TODO: mine a block
   let transactions = [];
   while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0) {
     transactions.push(mempool.pop());
   }
-
-  // Add new Block height prior to the new block being added
-  const newBlock = {
-    id: blocks.length,
-    transactions,
-  };
-  const blockString = JSON.stringify(newBlock);
-  let hash = SHA256(blockString);
-  blocks.push({ ...newBlock, hash });
+  const block = { id: blocks.length, transactions };
+  block.nonce = 0;
+  let hash;
+  while (true) {
+    hash = SHA256(JSON.stringify(block)).toString();
+    if (BigInt(`0x${hash}`) < TARGET_DIFFICULTY) {
+      break;
+    }
+    block.nonce++;
+  }
+  blocks.push({ ...block, hash });
 }
 
 module.exports = {
@@ -33,6 +32,6 @@ module.exports = {
   MAX_TRANSACTIONS,
   addTransaction,
   mine,
-  blocks,
   mempool,
+  blocks,
 };
