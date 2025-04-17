a40b6ed8207b453bc6168b5c2633454d0f420d57
molalign8468 <molaligngetahun229@gmail.com>
2025-04-18 01:28:52 +0300

    MERKLE TREE

--- MERKLE TREE/index.js
+++ MERKLE TREE/index.js
@@ -3,8 +3,17 @@ class MerkleTree {
     this.leaves = leaves;
     this.concat = concat;
   }
-  getRoot() {
-    return this.concat(this.leaves[0], this.leaves[1]);
+  getRoot(leaves = this.leaves) {
+    if (leaves.length === 1) {
+      return leaves[0];
+    }
+    const layer = [];
+    for (let i = 0; i < leaves.length; i += 2) {
+      const firstPart = leaves[i];
+      const nextPart = leaves[i + 1];
+      layer.push(this.concat(firstPart, nextPart));
+    }
+    return this.getRoot(layer);
   }
 }
 
