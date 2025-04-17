8bca9f9e02ed8ac18ca609b2f6215495266edfb8
molalign8468 <molaligngetahun229@gmail.com>
2025-04-18 01:38:12 +0300

    MERKLE TREE V4

--- MERKLE TREE/index.js
+++ MERKLE TREE/index.js
@@ -9,15 +9,37 @@ class MerkleTree {
     }
     const layer = [];
     for (let i = 0; i < leaves.length; i += 2) {
-      const firstPart = leaves[i];
-      const nextPart = leaves[i + 1];
-      if (nextPart) {
-        layer.push(this.concat(firstPart, nextPart));
+      const left = leaves[i];
+      const right = leaves[i + 1];
+      if (right) {
+        layer.push(this.concat(left, right));
       } else {
-        layer.push(firstPart);
+        layer.push(left);
       }
     }
     return this.getRoot(layer);
+  }
+  getProof(index, layer = this.leaves, proof = []) {
+    if (layer.length === 1) return proof;
+    const newLayer = [];
+    for (let i = 0; i < layer.length; i += 2) {
+      let left = layer[i];
+      let right = layer[i + 1];
+      if (!right) {
+        newLayer.push(left);
+      } else {
+        newLayer.push(this.concat(left, right));
+
+        if (i === index || i === index - 1) {
+          let isLeft = !(index % 2);
+          proof.push({
+            data: isLeft ? right : left,
+            left: !isLeft,
+          });
+        }
+      }
+    }
+    return this.getProof(Math.floor(index / 2), newLayer, proof);
   }
 }
 
