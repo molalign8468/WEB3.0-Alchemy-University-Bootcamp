57ece23118dc11724c6de1ef72e7d5c3ea8437d6
molalign8468 <molaligngetahun229@gmail.com>
2025-04-19 17:09:47 +0300

    Recursion Challenge

---
+++ FindNestedObject/index.js
@@ -0,0 +1,17 @@
+let obj = {
+  prop: {
+    prop: {
+      prop: 3,
+    },
+  },
+};
+
+function getDeeplyNestedValue(obj) {
+  if (typeof obj.prop === "object" && obj.prop !== null) {
+    return getDeeplyNestedValue(obj.prop);
+  } else {
+    return obj.prop;
+  }
+}
+
+console.log(getDeeplyNestedValue(i));
