38da105e76d27790af649a4494f9e3810e25c93b
molalign8468 <molaligngetahun229@gmail.com>
2025-04-24 23:43:55 +0300

    Added getTotalTransactions functionality

--- JSON RPC/getBlokeNo.js
+++ JSON RPC/getBlokeNo.js
@@ -34,6 +34,17 @@ async function getNonce(address) {
 
   return response.data.result;
 }
+async function getTotalTransactions(blockNumber) {
+  const response = await axios.post(url, {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getBlockByNumber",
+    params: [blockNumber, false],
+  });
+
+  return response.data.result.transactions.length;
+}
 getBlockNumber();
 geBalance("0x825964B04eb5f4c6156dFD48dD1a70082eF2cd56");
 console.log(getNonce("0x825964B04eb5f4c6156dFD48dD1a70082eF2cd56"));
+console.log(getTotalTransactions("getTotalTransactions"));
