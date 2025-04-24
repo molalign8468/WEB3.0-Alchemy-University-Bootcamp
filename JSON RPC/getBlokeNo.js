858c9090b9dcec4fe2bdc194b1034b6f6383390a
molalign8468 <molaligngetahun229@gmail.com>
2025-04-24 23:37:50 +0300

    Add get Balance functionality

--- JSON RPC/getBlokeNo.js
+++ JSON RPC/getBlokeNo.js
@@ -14,4 +14,15 @@ async function getBlockNumber() {
 
   return response.data.result;
 }
+const geBalance = async (address) => {
+  const response = await axios.post(url, {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getBalance",
+    params: [address],
+  });
+  console.log(response.data.result);
+  return response.data.result;
+};
 getBlockNumber();
+geBalance("0x825964B04eb5f4c6156dFD48dD1a70082eF2cd56");
